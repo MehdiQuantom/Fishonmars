80fe988cf7df78241d9561630050415b2ba64a57
Mehdi <37363392+MehdiQuantom@users.noreply.github.com>
2022-04-22 03:19:14 +0430

    Update

---
+++ Error/index.php
@@ -0,0 +1,32 @@
+<?php
+$output = <<<OUTPUT
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+  <meta charset="UTF-8">
+  <title>Error</title>
+  <link href='https://fonts.googleapis.com/css?family=Monoton' rel='stylesheet' type='text/css'><link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
+<link rel="stylesheet" href="./style.css">
+
+</head>
+<body>
+<div class="board">
+  <p id="error">
+    Error :/
+  </p>
+  <p id="code">
+    Get Premium <input type="button" value="Contact [MehdiQuantom]" onclick="window.open('https://t.me/Mehdi_Quantom','popUpWindow','height=500,width=400,left=100,top=100,resizable=yes,scrollbars=yes,toolbar=yes,menubar=no,location=no,directories=no, status=yes');">
+
+  </p>
+</div>
+
+
+
+
+  <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script><script  src="./script.js"></script>
+
+</body>
+</html>
+
+OUTPUT;
+echo $output;
