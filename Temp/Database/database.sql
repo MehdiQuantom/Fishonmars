dfa0d861fd47c51c3caf3dd775d1315d5c25dd68
Mehdi <37363392+MehdiQuantom@users.noreply.github.com>
2022-08-13 14:22:45 +0430

    Update

--- Temp/Database/database.sql
+++
@@ -1,7 +0,0 @@
-
-CREATE TABLE IF NOT EXISTS `notes` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `title` text COLLATE utf8_unicode_ci NOT NULL,
-  `text` text COLLATE utf8_unicode_ci NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
