296b2c32accdb8151f903039bb85aa8670bc8a55
Mehdi <37363392+MehdiQuantom@users.noreply.github.com>
2022-11-16 15:56:00 +0330

    Update

---
+++ QuantomInlineBrowser/BrowserSelection.php
@@ -0,0 +1,156 @@
+<?php
+$output = <<<OUTPUT
+<html>
+
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	
+	<title>Mehdi Quantom</title>
+
+	<style type="text/css">
+	
+
+body {
+	margin: 0;  padding: 0;  margin-bottom: 45px;  margin-top: 8px;
+	background: #e5e5e5;
+}
+body, td {
+	font: 16px "Trebuchet MS", Verdana, Arial, Helvetica, sans-serif;
+	}
+
+#subTitle {
+	background: #000;  color: #fff;  padding: 9px;  font-weight: bold; 
+	}
+
+#siteNavigation a, #siteNavigation .current {
+	font-weight: bold;  color: #648;
+	}
+#siteNavigation a:link    { text-decoration: none; }
+#siteNavigation a:visited { text-decoration: none; }
+
+#siteNavigation .current { background-color: #cdd; }
+
+#siteNavigation a:hover   { text-decoration: none;  background-color: #e5e5e5;  color: #e5e5e5; }
+#siteNavigation a:active  { text-decoration: none;  background-color: #ccc; }
+
+
+a:link    { text-decoration: underline;  color: #800; }
+a:visited { text-decoration: underline;  color: #000; }
+a:hover   { text-decoration: underline;  color: #c00; }
+a:active  { text-decoration: underline; }
+
+#pageContent {
+	clear: both;
+	border-bottom: 5px solid #000;
+	padding: 10px;  padding-top: 20px;
+	line-height: .65em;
+	background-image: url(gif.gif);
+	background-repeat: no-repeat;
+	background-position: bottom right;
+	}
+
+#pageContent, #siteNavigation {
+	background-color: #e5e5e5;
+	}
+
+
+.imgLeft  { float: left;   margin-right: 10px;  margin-bottom: 10px; }
+.imgRight { float: right;  margin-left: 10px;   margin-bottom: 10px; }
+
+hr { height: 1px;  color: #7f7f7f;  background-color: #7f7f7f;  margin-bottom: 10px; }
+
+h1 { margin: 0;  font-weight: bold;  font-size: 2em; }
+h2 { margin: 0;  font-weight: bold;  font-size: 1.6em; }
+h3 { margin: 0;  font-weight: bold;  font-size: 1.3em; }
+h4 { margin: 0;  font-weight: bold;  font-size: 1.18em; }
+
+.blak { background-color: #000; }
+.hide { display: none; }
+.tableWidth { min-width: 800px; }
+
+.tblRegular       { border-collapse: collapse; }
+.tblRegular td    { padding: 6px;  background-image: url(fade.gif);  border: 2px solid #99c; }
+.tblHeaderColor, .tblHeaderColor td { background: #99c; }
+.tblNoBorder td   { border: 0; }
+
+
+
+</style>
+
+</head>
+
+<body>
+<table width="76%" border="0" align="center" cellspacing="0" cellpadding="3" class="tableWidth">
+	<tr>
+	<td id="subTitle">Mehdi Quantom - %K42a112oaPA%</td>
+	</tr>
+</table>
+<table width="76%" border="0" align="center" cellspacing="0" cellpadding="0" class="tableWidth">
+<tr class="blak">
+<td>
+	<table width="100%" border="0" align="center" cellspacing="5" cellpadding="0">
+	<tr>
+	<td colspan="6"> 
+		<table width="100%" border="0" align="center" cellspacing="0" cellpadding="10">
+		<tr> 
+		<td id="pageContent"> 
+
+
+<h2 ALIGN=Center>Select Your Search Engine:</H2>
+  <table border="0" width="100%" cellspacing="25" cellpadding="0">
+		<TR>
+		  <TD BACKGROUND="fade.gif">
+                    &middot; <A HREF="https://jsp.daycat.space/-----https://www.google.com/?gws_rd=ssl">Google (Not Recommended!)</A>
+   		  </TD>
+		</TR>
+		<TR>
+		  <TD BACKGROUND="fade.gif">
+                    &middot; <A HREF="https://jsp.daycat.space/-----https://www.bing.com/">Bing</A>
+   		  </TD>
+		</TR>
+		<TR>
+		  <TD BACKGROUND="fade.gif">
+                    &middot; <A HREF="https://jsp.daycat.space/-----https://consent.yahoo.com">Yahoo</A>
+   		  </TD>
+		</TR>
+		<TR>
+		  <TD BACKGROUND="fade.gif">
+                    &middot; <A HREF="https://jsp.daycat.space/-----https://duckduckgo.com/">DuckDuckGo</A>
+   		  </TD>
+		</TR>
+		<TR>
+		  <TD BACKGROUND="fade.gif">
+                    &middot; <A HREF="https://jsp.daycat.space/-----http://www.baidu.com/">Baidu</A>
+   		  </TD>
+		</TR>
+	</TABLE>
+	<BR>
+	<H6 ALIGN="left">
+         <I>Quantom Services [2022]</I>
+	</H6>
+	
+
+		</td>
+		</tr>
+		</table>
+	</td>
+	</tr>
+	</table>
+</td>
+</tr>
+</table>
+
+<table width="76%" border="0" align="center" valign="bottom" cellspacing="0" cellpadding="0">
+	<tr>
+	<td id="footer"><small>&copy; Mehdi Quantom</small></td>
+	</tr>
+</table>
+
+</body>
+
+</html>
+
+
+
+OUTPUT;
+echo $output;
