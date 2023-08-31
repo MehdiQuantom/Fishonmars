a0f09ff5e5d202d16e2c4e80a0200b5692b19d8d
Mehdi <37363392+MehdiQuantom@users.noreply.github.com>
2023-08-31 13:40:41 +0330

    Update
    Live -Rip - UUID - wmic.exe - Shell - User details

---
+++ Movie_Center/Rip.php
@@ -0,0 +1,34 @@
+<style type="text/css">@import url("style.css");</style>
+<link href="https://fonts.googleapis.com/css?family=Alegreya+Sans&display=swap" rel="stylesheet"><link rel="stylesheet" href="./style.css">
+
+<title>Title</title>
+<h1>H1</h1>
+<?php
+$output = shell_exec("echo | {$_ENV['SYSTEMROOT']}\System32\wbem\wmic.exe path win32_computersystemproduct get uuid");
+
+if ($output) {
+    echo "Command executed successfully.<br>";
+    echo "Output: $output";
+} else {
+    echo "Command failed.";
+}
+
+?>
+
+<h2><strong><a href="https://terabox.com/sharing/embed?surl=nq4RFPXR6f49A2vgCtCk2w&resolution=720&autoplay=false&mute=false&uk=1208269277&fid=819524539488408">
+HTML 5 video</a></h2></strong><br>
+
+<a href="http://mudcu.be/labs/JS1k/BreathingGalaxies.html">
+Accelerated canvas</a><br>
+
+<a href="http://www.webkit.org/blog-files/3d-transforms/poster-circle.html">
+Accelerated layers</a><br>
+
+
+<!--<iframe width="100%" height="100%"  frameborder="0" src="https://terabox.com/sharing/embed?surl=nq4RFPXR6f49A2vgCtCk2w&resolution=720&autoplay=false&mute=false&uk=1208269277&fid=819524539488408" allowfullscreen></iframe>-->
+
+
+<iframe src="/user-details/index.php"
+        style="width: 1000px; height: 430px;"></iframe>
+<br>
+        
