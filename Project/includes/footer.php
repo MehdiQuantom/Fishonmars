f1f802984e01dc4dce13346b1e78828fbf89fb34
Mehdi <37363392+MehdiQuantom@users.noreply.github.com>
2023-12-17 15:54:14 +0330

    Update /|\

---
+++ Project/includes/footer.php
@@ -0,0 +1,4 @@
+<footer class="footer">
+    <div class="container text-center"><span class="text-muted"><b>Copyright&copy;Mehdi Quantom | All Rights Reserved | Contact Us: +989353511653</b></span></div>
+</footer>
+    
